788aa86d91003038c04a46ecfc14ccf90e63ceea
Andrea Azzola <andrea.azzola@webresults.it>
2014-04-15 18:51:25 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace aa.NLogSQlite
+{
+    using System;
+    using System.Configuration;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Data.SQLite;
+    using System.IO;
+    using NLog;
+
+    class Program
+    {
+        static Logger log = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            EnsureDB();
+
+            log.Info("Logging like a boss");
+        }
+
+        static void EnsureDB()
+        {
+            if (File.Exists("Log.db3"))
+                return;
+
+            using (SQLiteConnection connection = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SQLite"].ToString()))
+            using (SQLiteCommand command = new SQLiteCommand(
+                "CREATE TABLE Log (Timestamp TEXT, Loglevel TEXT, Logger TEXT, Callsite TEXT, Message TEXT)",
+                connection))
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
